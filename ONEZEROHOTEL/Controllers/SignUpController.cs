e8be327e01313153aafd4cd6a1aa7c25e7d9e7c4
Estarmani <zamaniesther@gmail.com>
2023-06-27 08:24:16 +0100

    created sign up page

---
+++ ONEZEROHOTEL/Controllers/SignUpController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ONEZEROHOTEL.Controllers
+{
+    public class SignUpController : Controller
+    {
+        private readonly ILogger<SignUpController> _logger;
+
+        public SignUpController(ILogger<SignUpController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
